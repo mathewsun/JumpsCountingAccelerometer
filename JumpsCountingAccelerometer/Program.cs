50e4455b0679578054e2dcd3484b3dc15403bf1c
Mathew <mathew.dubrovin@gmail.com>
2024-04-14 10:30:10 +0300

    new project

---
+++ JumpsCountingAccelerometer/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace JumpsCountingAccelerometer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
