7a9c744a6ba653d9ac4fe288a1a932fc690d8c48
Mathew <mathew.dubrovin@gmail.com>
2024-04-14 10:32:56 +0300

    counting jumps

--- JumpsCountingAccelerometer/Program.cs
+++ JumpsCountingAccelerometer/Program.cs
@@ -1,10 +1,28 @@
-﻿namespace JumpsCountingAccelerometer
+﻿using Newtonsoft.Json.Linq;
+
+var json = await File.ReadAllTextAsync("jump.json");
+var jObject = JObject.Parse(json);
+
+var accelerationData = jObject["acceleration_data"];
+var zValues = accelerationData.Select(x => x["acc_z"].Value<double>()).ToArray();
+
+Console.WriteLine(CalculateJumps(zValues, 40));
+
+static int CalculateJumps(double[] values, int windowSize)
 {
-    internal class Program
+    var index = 0;
+    var jumpCount = 0;
+
+    while (index < values.Length - 1)
     {
-        static void Main(string[] args)
+        if (values[index] < -8)
         {
-            Console.WriteLine("Hello, World!");
+            jumpCount++;
+
+            index += windowSize;
         }
+        else { index++; }
     }
+
+    return jumpCount;
 }
